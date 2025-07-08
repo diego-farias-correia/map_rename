2854778749375176b377b42f844367643d33c723
diego-farias-correia <descode.techdev@gmail.com>
2025-07-08 17:33:02 -0300

    Final model code
    This commit contains the final model code to rename map files

--- map_ibge.py
+++ map_ibge.py
@@ -5,8 +5,6 @@ from pdf2image import convert_from_path
 from pathlib import Path
 from PIL import Image
 
-import file_map_management
-
 ROOT_FOLDER = Path(__file__).parent
 PDF_FOLDER = ROOT_FOLDER / "pdf"
 JPG_FOLDER = ROOT_FOLDER / "jpg"
@@ -32,7 +30,7 @@ def street_satellite(path_jpg):
         red, blue, green = cv.imread(path_jpg)[cut, 700]
         if red == blue == green:
             count_pxl += 1
-        if count_pxl > 4:
+        if count_pxl > 3:
             return 'm'
         return 'i'
 
@@ -50,7 +48,6 @@ def try_geocode(compose):
     return "Error-name"
 
 
-
 folder_list = list(PDF_FOLDER.iterdir())
 total_folder = len(folder_list)
 
@@ -64,6 +61,7 @@ for folder_progress, day_folder in enumerate(folder_list, start=1):
     for pdf_progress, op_folder in enumerate(list_file, start=1):
 
         print(f"Actual progress: {(pdf_progress/total_files)*100}% of files...")
+        print(f" Work in file: {op_folder.name}")
 
         white_page = convert_pdf(str(op_folder))
         if white_page is True:
@@ -85,25 +83,64 @@ for folder_progress, day_folder in enumerate(folder_list, start=1):
             new_name_file = f"{geocode}-{image_type}"
 
         except IndexError:
-            new_name_file = "Error-pag2"
+            geocode = "Error-pag2"
             print("Error in the second page! SCAN IN STANDARD POSITION")
 
         new_name_file = f"{geocode}-{image_type}"
-        
-        if new_name_file in occurrence_list:
-            new_name_file = f"{new_name_file}-{occurrence_list.count(new_name_file)}"
+
+
+        if new_name_file.split('-')[0] in occurrence_list:
+            new_name_file = f"{new_name_file}-{occurrence_list.count(new_name_file.split('-')[0])}"
         
         occurrence_list.append(new_name_file.split('-')[0])
-
+        
         rename_sintaxe = op_folder.parent / f"{new_name_file}.pdf"
         op_folder.rename(rename_sintaxe)
         print(f"The file {new_name_file} has succesfull renamed")
 
-        if new_name_file[:6] == "Error":
+        if new_name_file[:5] == "Error":
+            print(str(op_folder.parent / f"{new_name_file}.pdf"))
+            print(str(ERROR_FOLDER / f"{new_name_file}.pdf"))
             shutil.move(op_folder.parent / f"{new_name_file}.pdf", ERROR_FOLDER / f"{new_name_file}.pdf")
             print(f"The error file {new_name_file} is moved to {ERROR_FOLDER}.")
 
 for files in JPG_FOLDER.iterdir():
     files.unlink()
 
-file_map_management
+print("Iniciando geração de relatórios...")
+
+import json
+from datetime import date
+
+EXE_FOLDER = ROOT_FOLDER / "files_exe"
+RELATORY_FOLDER = EXE_FOLDER / "relatory"
+
+
+def create_file(name_file=""):
+    with open(str(RELATORY_FOLDER / name_file), 'w'):
+        print(f"The new relatory file {name_file} has created.")    
+
+
+def insert_file(*args, name_file=""):
+    with open(str(RELATORY_FOLDER / name_file), 'a') as file:
+        for arg in args:
+            file.write(f"{arg}\n")
+
+
+set_geocode = {geocode[:15] for geocode in occurrence_list if geocode[:5] != "Error"}
+with open(str(EXE_FOLDER / "base.json"), 'r') as file:
+    dict_base = json.load(file)
+
+training_map = [geocode for geocode in set_geocode if geocode[:2] != '25']
+csv_list = [dict_base[geocode] for geocode in set_geocode if geocode[:2] == '25']
+
+rel_name_file = f"RELATORY-{date.today()}.txt"
+create_file(rel_name_file)
+insert_file(*csv_list, name_file=rel_name_file)
+
+if len(training_map)>1:
+    tm_name_file = f"TRAININGMAP-{date.today()}.txt"
+    create_file(tm_name_file)
+    insert_file(*training_map, tm_name_file)
+
+print(f"Processo de criação concluído. Seguir para {RELATORY_FOLDER} para verificar os relatórios criados")
